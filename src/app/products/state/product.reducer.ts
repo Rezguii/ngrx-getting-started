d1445f0ca3c39cfdbce7834153ffc44b2632aa71
Baha_eddine <bahaeddine.razgui@rhis-solutions.com>
2020-10-06 07:11:22 +0100

    correct readme file & initial state

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -1,7 +1,7 @@
 import {createAction, createReducer, on} from "@ngrx/store";
 
 export const productRedurcer = createReducer(
-  {showProductCode: true},
+  {showProductCode: false},
   on(createAction('[Product] Toggle Product code'), state => {
     console.log('original state: ', JSON.stringify(state));
     return {
