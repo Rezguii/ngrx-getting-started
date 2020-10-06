62efd004840a53a4fc71a31f98ff08c527f43920
Rezguii <rezguibahaeddinne@gmail.com>
2020-10-06 07:34:26 +0100

    delete undesired console.log

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -3,7 +3,6 @@ import {createAction, createReducer, on} from "@ngrx/store";
 export const productRedurcer = createReducer(
   {showProductCode: false},
   on(createAction('[Product] Toggle Product code'), state => {
-    console.log('original state: ', JSON.stringify(state));
     return {
       ...state,
       showProductCode: !state.showProductCode
